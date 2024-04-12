1daed0352ef441b32b684995476c24d00a86483d
TraxDinosaur <165242235+TraxDinosaur@users.noreply.github.com>
2024-04-12 23:08:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,19 @@
         document.getElementById('contact-form-container').style.display = 'flex';
     });
 
+    // New Logo Event Listener
+    const newLogo = document.getElementById('new-logo');
+    newLogo.addEventListener('click', function(event) {
+        event.preventDefault();
+        togglePopupLogos();
+    });
+
+    // Toggle Popup Logos
+    function togglePopupLogos() {
+        const popupLogos = document.querySelector('.popup-logos');
+        popupLogos.classList.toggle('show');
+    }
+
     const contactForm = document.getElementById('contact-form');
 
     // Enable typing in input boxes
@@ -19,8 +32,8 @@
         const message = document.getElementById('message').value;
 
         // If You Will Use This Token Or Channel Id Then Congrats! You Are A Failiure !!
-        const botToken = '6641775231:AAFGXNrUJxYYu9z5BfJ9xLioiWZ4f8nteOY'; 
-        const chatId = '-1001839766283'; 
+        const botToken = '6641775231:AAFGXNrUJxYYu9z5BfJ9xLioiWZ4f8nteOY';
+        const chatId = '-1001839766283';
         const text = `Name: ${name}\nEmail: ${email}\nMessage: ${message}`;
 
         fetch(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(text)}`)
@@ -58,8 +71,13 @@
     document.addEventListener('click', function(event) {
         const contactFormContainer = document.getElementById('contact-form-container');
         const contactForm = document.querySelector('.contact-form');
-        if (!contactForm.contains(event.target) && !document.getElementById('profile-logo').contains(event.target)) {
+        if (!contactForm.contains(event.target) && !document.getElementById('profile-logo').contains(event.target) && !document.getElementById('new-logo').contains(event.target)) {
             contactFormContainer.style.display = 'none';
+            // Hide Popup Logos if visible
+            const popupLogos = document.querySelector('.popup-logos');
+            if (popupLogos.classList.contains('show')) {
+                popupLogos.classList.remove('show');
+            }
         }
     });
 })();
